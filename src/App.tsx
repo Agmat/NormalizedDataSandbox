01b221cf81c80212cc91df8f64623e51f181137d
Gabriel Cabanes <gcabanes@meilleursagents.com>
2021-07-12 11:20:13 +0200

    feat(persist): persist store to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import './App.css';
 
 import { Typography } from '@material-ui/core';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchAllTodos } from './app/reducers/todosReducer/todosReducer';
+import { RootState } from './app/reducers/rootReducer';
+import { fetchAllTodos, todosSelectors } from './app/reducers/todosReducer/todosReducer';
 import AddTodo from './components/AddTodo/AddTodo';
 import TodosList from './components/TodosList/TodosList';
 const App = () => {
   const dispatch = useDispatch();
+  const totalTodos = useSelector(
+    (state: RootState) => todosSelectors.selectTotal(state),
+    () => false,
+  );
   useEffect(() => {
-    dispatch(fetchAllTodos());
+    if (totalTodos === 0) dispatch(fetchAllTodos());
   }, []);
 
   return (
